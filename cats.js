b4d8cf9fc0256e776f2a62b1270d96fbd8f7ebe2
Nils <nils.mehlhorn.fr@gmail.com>
2023-03-16 13:39:35 +0100

    clean populate and cats.js

--- cats.js
+++ cats.js
@@ -34,10 +34,61 @@ function displayRestaurants(restaurants) {
     restaurantListDiv.removeChild(restaurantListDiv.firstChild);
   }
 
+  /*  // Create an HTML element for each restaurant
+   restaurants.forEach(restaurant => {
+     const restaurantDiv = document.createElement('div');
+     restaurantDiv.textContent = restaurant.title;
+     restaurantListDiv.appendChild(restaurantDiv);
+   }); */
+
+
+
   // Create an HTML element for each restaurant
   restaurants.forEach(restaurant => {
-    const restaurantDiv = document.createElement('div');
-    restaurantDiv.textContent = restaurant.title;
-    restaurantListDiv.appendChild(restaurantDiv);
+    // Create a <ul> element for the restaurant
+    const restaurantUl = document.createElement('ul');
+
+    // Create a <li> element for each parameter of the restaurant and append it to the <ul>
+    const titleLi = document.createElement('li');
+    titleLi.textContent = restaurant.title;
+    restaurantUl.appendChild(titleLi);
+
+    // adress li
+    const addressLi = document.createElement('li');
+    addressLi.textContent = restaurant.address;
+    restaurantUl.appendChild(addressLi);
+
+
+    // phone
+    const phoneLi = document.createElement('li');
+    phoneLi.textContent = restaurant.phone;
+    restaurantUl.appendChild(phoneLi);
+
+
+    //url map li
+    const urlLi = document.createElement('li');
+    const urlLink = document.createElement('a'); // create a new hyperlink element
+    urlLink.href = restaurant.url; // set the href attribute to the URL of the website
+    urlLink.textContent = "Google Map"; // set the link text to the website URL
+    urlLi.appendChild(urlLink); // append the hyperlink element to the li element
+    restaurantUl.appendChild(urlLi);
+
+
+    //website adress as link li
+    const websiteLi = document.createElement('li');
+    const websiteLink = document.createElement('a'); // create a new hyperlink element
+    websiteLink.href = restaurant.website; // set the href attribute to the URL of the website
+    websiteLink.textContent = restaurant.title; // set the link text to the website URL
+    websiteLi.appendChild(websiteLink); // append the hyperlink element to the li element
+    restaurantUl.appendChild(websiteLi);
+
+
+    const priceLi = document.createElement('li');
+    priceLi.textContent = restaurant.price;
+    restaurantUl.appendChild(priceLi);
+
+    // Append the <ul> element to the container for the restaurant list
+    restaurantListDiv.appendChild(restaurantUl);
   });
+
 }
