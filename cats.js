670a4d0b514269d97e2c37ebf44525fc08f6f016
Amaury CLOT <amauryclt@gmail.com>
2023-03-22 09:50:22 +0100

    commentaire retiré

--- cats.js
+++ cats.js
@@ -1,6 +1,6 @@
 import restaurants from "/scrapping/rest.js";
 
-// Get a list of all categories
+
 const categories = [];
 restaurants.forEach(restaurant => {
   if (!categories.includes(restaurant.categoryName)) {
@@ -8,7 +8,7 @@ restaurants.forEach(restaurant => {
   }
 });
 
-// Create a button for each category
+
 const categoryButtons = categories.map(category => {
   const button = document.createElement('button');
   button.textContent = category;
@@ -18,37 +18,32 @@ const categoryButtons = categories.map(category => {
 
 console.log(categories);
 
-// Add the category buttons to the page
+
 const categoriesDiv = document.getElementById('categories');
 categoryButtons.forEach(button => categoriesDiv.appendChild(button));
 
-// Filter restaurants based on the selected category
+
 function filterRestaurants(category, data) {
   const filteredRestaurants = data.filter(restaurant => restaurant.categoryName === category);
   displayRestaurants(filteredRestaurants);
 }
 
-// Display a list of restaurants
+
 function displayRestaurants(restaurants) {
   const restaurantListDiv = document.getElementById('restaurantList');
 
-  // Clear the previous list of restaurants
+
   while (restaurantListDiv.firstChild) {
     restaurantListDiv.removeChild(restaurantListDiv.firstChild);
   }
 
-  /*  // Create an HTML element for each restaurant
-   restaurants.forEach(restaurant => {
-     const restaurantDiv = document.createElement('div');
-     restaurantDiv.textContent = restaurant.title;
-     restaurantListDiv.appendChild(restaurantDiv);
-   }); */
 
 
 
-  // Create an HTML element for each restaurant
+
+
   restaurants.forEach(restaurant => {
-    // Create a <ul> element for the restaurant
+
     const restaurantUl = document.createElement('ul');
     restaurantUl.classList.add('restaurantUl');
 
@@ -63,7 +58,7 @@ function displayRestaurants(restaurants) {
     restaurantUl.appendChild(titleLi);
 
 
-    // Append the <ul> element to the container for the restaurant list
+
     restaurantListDiv.appendChild(restaurantUl);
   });
 
