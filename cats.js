dc29241541c4fe13f4cfcfa466fbab51e3b58f5c
Nils <nils.mehlhorn.fr@gmail.com>
2023-03-28 09:02:29 +0200

    modale windows faite

--- cats.js
+++ cats.js
@@ -1,39 +1,33 @@
-import restaurants from "/scrapping/rest.js";
+import restaurants from "./scrapping/rest.js";
 
-//commentaire
 const categories = [];
-restaurants.forEach(restaurant => {
+restaurants.forEach((restaurant) => {
   if (!categories.includes(restaurant.categoryName)) {
     categories.push(restaurant.categoryName);
   }
-
 });
-const categoryButtons = categories.map(category => {
-  const button = document.createElement('button');
+
+const categoryButtons = categories.map((category) => {
+  const button = document.createElement("button");
   button.textContent = category;
-  button.addEventListener('click', () => filterRestaurants(category, restaurants));
+  button.addEventListener("click", () =>
+    filterRestaurants(category, restaurants)
+  );
   return button;
 });
 
+const categoriesDiv = document.getElementById("categories");
+categoryButtons.forEach((button) => categoriesDiv.appendChild(button));
 
-
-console.log(categories);
-
-//commentaire
-const categoriesDiv = document.getElementById('categories');
-categoryButtons.forEach(button => categoriesDiv.appendChild(button));
-
-//commentaire
 function filterRestaurants(category, data) {
-  const filteredRestaurants = data.filter(restaurant => restaurant.categoryName === category);
+  const filteredRestaurants = data.filter(
+    (restaurant) => restaurant.categoryName === category
+  );
   displayRestaurants(filteredRestaurants);
 }
 
-
-
-//commentaire
 function displayRestaurants(restaurants) {
-  const restaurantListDiv = document.getElementById('restaurantList');
+  const restaurantListDiv = document.getElementById("restaurantList");
 
   while (restaurantListDiv.firstChild) {
     restaurantListDiv.removeChild(restaurantListDiv.firstChild);
@@ -43,53 +37,50 @@ function displayRestaurants(restaurants) {
     return `src/${slug}/2.jpg`;
   }
 
-  restaurants.forEach(restaurant => {
-    const restaurantUl = document.createElement('ul');
-    restaurantUl.classList.add('restaurantUl');
-    const titleLi = document.createElement('li');
-    const imageLi = document.createElement('img');
-    const titleLink = document.createElement('a');
+  restaurants.forEach((restaurant) => {
+    const restaurantUl = document.createElement("ul");
+    restaurantUl.classList.add("restaurantUl");
+    const titleLi = document.createElement("li");
+    const imageLi = document.createElement("img");
+    const titleLink = document.createElement("a");
     imageLi.src = getUrlFromSlug(restaurant.slug);
     titleLink.textContent = restaurant.title;
-    titleLink.href = '#';
-    titleLink.classList.add('restaurant-title');
+    titleLink.href = "#";
+    titleLink.classList.add("restaurant-title");
     titleLi.appendChild(titleLink);
     restaurantUl.appendChild(imageLi);
     restaurantUl.appendChild(titleLi);
     restaurantListDiv.appendChild(restaurantUl);
+    titleLink.addEventListener("click", toggleDetailsPopup);
 
-    titleLink.addEventListener("click", toggleDetailsPopup);//eventlistener
-    // function intermediare to use function restdetailsPopup
-    // Add a variable to keep track of whether the restaurant details are currently displayed
     let detailsDisplayed = false;
 
     function toggleDetailsPopup(e) {
+      console.log('toggleDetailsPopup called');
+      console.log('detailsDisplayed:', detailsDisplayed);
       const siblings = e.target.parentNode;
       if (detailsDisplayed) {
-        // If the details are currently displayed, remove them and update the variable
         siblings.removeChild(siblings.lastChild);
         detailsDisplayed = false;
       } else {
-        // If the details are not currently displayed, call restDetailsPopup() and update the variable
         restDetailsPopup(siblings);
+        console.log('restDetailsPopup called');
         detailsDisplayed = true;
       }
     }
 
-    //function to show the restaurant details in a popup window
     function restDetailsPopup(siblings) {
-
-      // Create a div to hold the restaurant details
       const detailsDiv = document.createElement("div");
+      console.log('restDetailsPopup called');
       detailsDiv.classList.add("restaurant-details");
 
-      // Add the restaurant details to the div
       const addressLi = document.createElement("li");
       addressLi.textContent = restaurant.address;
       detailsDiv.appendChild(addressLi);
 
       const phoneLi = document.createElement("li");
       phoneLi.textContent = restaurant.phone;
+      phoneLi.classList.add("phone");
       detailsDiv.appendChild(phoneLi);
 
       const urlLi = document.createElement("li");
@@ -98,33 +89,42 @@ function displayRestaurants(restaurants) {
       urlLink.textContent = "Google Map";
       urlLi.appendChild(urlLink);
       detailsDiv.appendChild(urlLi);
+
       const mapLi = document.createElement("li");
       const mapDiv = document.createElement("div");
       mapDiv.classList.add("restaurant-map");
-      const mapUrl = `https://www.google.com/maps/embed/v1/place?key=AIzaSyB4TKawJVgNVaaKOUwbg2MAcQywVSpNKRc&q=${encodeURIComponent(restaurant.address)}`;
+      const mapUrl = `https://www.google.com/maps/embed/v1/place?key=AIzaSyB4TKawJVgNVaaKOUwbg2MAcQywVSpNKRc&q=${encodeURIComponent(
+        restaurant.address
+      )}`;
       mapDiv.innerHTML = `<iframe width="100%" height="300" frameborder="0" style="border:0" src="${mapUrl}" allowfullscreen></iframe>`;
       mapLi.appendChild(mapDiv);
       detailsDiv.appendChild(mapLi);
 
       const websiteLi = document.createElement("li");
       const websiteLink = document.createElement("a");
+      websiteLink.classList.add("website");
       websiteLink.href = restaurant.website;
-      websiteLink.textContent = restaurant.title;
+      websiteLink.textContent = "Website";
       websiteLi.appendChild(websiteLink);
       detailsDiv.appendChild(websiteLi);
 
-      const priceLi = document.createElement("li");
-      priceLi.textContent = restaurant.price;
-      detailsDiv.appendChild(priceLi);
+      siblings.appendChild(detailsDiv);
 
-      // Create a popup window to display the details
-      const popupWindow = window.open("", "restaurantDetailsPopup", "width=400,height=300");
-      popupWindow.document.body.appendChild(detailsDiv);
+      const websiteModal = new Modal('Website', 'website-modal');
+      websiteLink.addEventListener('click', () => {
+        websiteModal.open();
+        const iframe = document.createElement('iframe');
+        iframe.src = restaurant.website;
+        iframe.width = '100%';
+        iframe.height = '500';
+        websiteModal.content.appendChild(iframe);
+      });
 
+      websiteLi.appendChild(websiteLink);
+      detailsDiv.appendChild(websiteLi);
 
-
-
+      siblings.appendChild(detailsDiv);
     }
-    restaurantListDiv.appendChild(restaurantUl);
-  })
+
+  });
 }
